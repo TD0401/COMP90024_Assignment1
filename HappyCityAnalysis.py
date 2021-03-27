e727027fa672773a24266d3ee9e13bb748fcaa08
Trina Dey <trina.dumca@gmail.com>
2021-03-27 14:41:02 +0530

    added grid parsing logic and test cases

--- HappyCityAnalysis.py
+++ HappyCityAnalysis.py
@@ -1,6 +1,30 @@
 import json
 import os
 
+
+def read_sentiment():
+    raw_file = "AFINN.txt"
+    word_store = {}
+
+    with open(raw_file, "r") as file_in:
+        for line in file_in:
+            s = line.replace("\t", "#^#")
+            alist = s.split("#^#")
+            word_store[alist[0]] = int(alist[1])
+    return word_store
+
+def melb_grid():
+    grid_dict = {}
+    with open("melbGrid2.json", "r") as json2py:
+        obj = json.load(json2py)
+        for item in obj['features']:
+            id = item['properties']['id']
+            grid_dict[id] = item['properties']
+            grid_dict[id]['score'] = 0
+
+    return grid_dict
+
+
 """ This functions reads the file from a start index byte to an offset defined so that number of lines in memory are restricted
     The readlines will read lines with total bytesize < size_byte
     Parameters
@@ -16,7 +40,7 @@ import os
 """
 
 
-def read_files(file_ptr, file_size, start_index=0, size_byte=10000):
+def read_files(file_ptr, file_size, map_grid,sentiment_dict, start_index=0, size_byte=10000 ):
     try:
         file_ptr.seek(start_index)
         lines = file_ptr.readlines(size_byte)
@@ -31,7 +55,7 @@ def read_files(file_ptr, file_size, start_index=0, size_byte=10000):
                     first_line = True
                 # not reading last line, parse all json in between
                 elif file_ptr.tell() < file_size or count_lines < num_lines:
-                    parse_json(line)
+                    parse_json(line, map_grid,sentiment_dict)
             except:
                 print("error in parsing particular line")
 
@@ -39,23 +63,46 @@ def read_files(file_ptr, file_size, start_index=0, size_byte=10000):
         print("Error in reading file and parsing data")
 
 
-def parse_json(line):
+def parse_json(line, map_grid,sentiment_dict):
     decoded_line = line.decode("UTF-8").replace("\n", "").replace("\r","")
     if decoded_line.endswith(","):
         decoded_line = decoded_line[:-1]
     data = json.loads(decoded_line)
-    tweet_id = data["id"]
-    tweet_lat = data["doc"]["coordinates"]["coordinates"][0]
-    tweet_lng = data["doc"]["coordinates"]["coordinates"][1]
+    tweet_lng = data["doc"]["coordinates"]["coordinates"][0]
+    tweet_lat = data["doc"]["coordinates"]["coordinates"][1]
     tweet_text = data["doc"]["text"]
+    cellId = find_cell_id(tweet_lat, tweet_lng, map_grid)
+    print("tweet_lat: " + str(tweet_lat) + ", tweet_lng: " + str(tweet_lng) + " , cellId:" + cellId)
+    if cellId != '':
+        print('write cell score here')
+        #score = getScore(text)
+        #addDataToCellList(cellId, score)
 
-    #cellId = getCellId(tweet_lat, tweet_lng)
-    #score = getScore(text)
-    #addDataToCellList(cellId, score)
 
-    #CellVsScore -> dictionary where key -> cellid, value -> object (cellid, nelat, nelng, swlat,swlng score)
-    print("id: " + str(tweet_id) + ", lat: " + str(tweet_lat) + ", lng: " + str(tweet_lng) + ", text: " + str(tweet_text))
-    #parse and put into data structure when defined, data strucutre should be defined at global level or local is to be checked during parallelization
+def find_cell_id(lat,lng, map_grid):
+    cell_id = ''
+    prev_ymin =''
+    left_populated = False
+    for coord in map_grid.values():
+        #within cell
+        if lng > coord['xmin'] and lng < coord['xmax'] and lat > coord['ymin']  and lat < coord['ymax']:
+            cell_id = coord['id']
+
+        # on vertical bounds go left
+        if lng == coord['xmax'] and lat >= coord['ymin']  and lat <= coord['ymax']:
+            if prev_ymin == '' or prev_ymin < coord['ymin']:
+                prev_ymin= coord['ymin']
+                cell_id = coord['id']
+                left_populated = True
+        if lng == coord['xmin']  and lat >= coord['ymin'] and lat <= coord['ymax'] and not left_populated:
+            cell_id = coord['id']
+        # on horizontal bounds go bottom
+        if lat == coord['ymin']  and lng >= coord['xmin'] and lng <= coord['xmax'] and not left_populated:
+            cell_id = coord['id']
+        if lat == coord['ymax']   and lng >= coord['xmin'] and lng <= coord['xmax'] and not left_populated:
+            cell_id = coord['id']
+    return cell_id
+
 
 
 """ TODO: 1. open file and iterate until eof is found
@@ -64,13 +111,17 @@ def parse_json(line):
           4. stop reading when file size has reached current pointer index
           5. while parsing ignore first and last line  -- write a parse json function
 """
-
-with open("/Users/trinadey/tinyTwitter.json", "rb") as file:
-    file_size = os.path.getsize("/Users/trinadey/tinyTwitter.json")
-    curr_index = file.tell()
-    while curr_index < file_size:
-        #parallelize read files get output, reduce it
-        read_files(file, file_size, curr_index)
+def main():
+    sentiment_dict = read_sentiment()
+    map_grid = melb_grid()
+    file_name= "tinyTwitter.json"
+    with open(file_name, "rb") as file:
+        file_size = os.path.getsize(file_name)
         curr_index = file.tell()
+        while curr_index < file_size:
+            #parallelize read files get output, reduce it
+            read_files(file, file_size,  map_grid,sentiment_dict , curr_index)
+            curr_index = file.tell()
 
 
+main()
---
+++ HappyCityAnalysisTest.py
@@ -0,0 +1,96 @@
+from HappyCityAnalysis import  find_cell_id
+
+def test_find_grid_cell2():
+    map = {}
+    map['C1'] = {'id': 'C1', 'xmin': -3.0, 'xmax': -1.5, 'ymin': 0, 'ymax': 1.5, 'score': 0}
+    map['C7'] = {'id': 'C7', 'xmin': 0.0, 'xmax': 1.5, 'ymin': -1.5, 'ymax': 0, 'score': 0}
+    map['C4'] = {'id': 'C4', 'xmin': 1.5, 'xmax': 3.0, 'ymin': 0, 'ymax': 1.5, 'score': 0}
+    map['C10'] = {'id': 'C10', 'xmin': -1.5, 'xmax': 0.0, 'ymin': -3.0, 'ymax': -1.5, 'score': 0}
+    map['C5'] = {'id': 'C5', 'xmin': -3.0, 'xmax': -1.5, 'ymin': -1.5, 'ymax': 0, 'score': 0}
+    map['C8'] = {'id': 'C8', 'xmin': 1.5, 'xmax': 3.0, 'ymin': -1.5, 'ymax': 0, 'score': 0}
+    map['C3'] = {'id': 'C3', 'xmin': 0.0, 'xmax': 1.5, 'ymin': 0, 'ymax': 1.5, 'score': 0}
+    map['C2'] = {'id': 'C2', 'xmin': -1.5, 'xmax': 0, 'ymin': 0, 'ymax': 1.5, 'score': 0}
+    map['C9'] = {'id': 'C9', 'xmin': -3.0, 'xmax': -1.5, 'ymin': -3.0, 'ymax': -1.5, 'score': 0}
+    map['C11'] = {'id': 'C11', 'xmin': 0, 'xmax': 1.5, 'ymin': -3.0, 'ymax': -1.5, 'score': 0}
+    map['C6'] = {'id': 'C6', 'xmin': -1.5, 'xmax': 0.0, 'ymin': -1.5, 'ymax': 0, 'score': 0}
+    map['C12'] = {'id': 'C12', 'xmin': 1.5, 'xmax': 3.0, 'ymin': -3.0, 'ymax': -1.5, 'score': 0}
+    map['C13'] = {'id': 'C13', 'xmin': -1.5, 'xmax': 0, 'ymin':  1.5, 'ymax': 3, 'score': 0}
+    map['C14'] = {'id': 'C14', 'xmin': 0, 'xmax': 1.5, 'ymin': -4.5, 'ymax': -3.0, 'score': 0}
+    map['C15'] = {'id': 'C15', 'xmin': -1.5, 'xmax': 0, 'ymin': -4.5, 'ymax': -3.0, 'score': 0}
+
+    '''points on outer boundary of grids which means
+        a. either left is null between left and right 
+        b. either right is null between right and left
+        c. either top is null between top and bottom
+        d. either bottom is null between top and bottom 
+        all passed
+    '''
+    A = [ -3 , 1.2]
+    B = [-2.7 , 1.5]
+    F = [ -3 , -2.8]
+    G = [ -1.8, -3.0]
+    H = [2.1, -3.0]
+    I = [3, -2.1]
+    J = [ 3, 1.1]
+    K = [2.0, 1.5]
+
+    #points inside the grid sharing boundaries - correct
+    C = [-2.2, 0]
+    D = [0, 1.1]
+    E = [-0.8, 0]
+    Y = [-1.1, 1.5]
+
+    #point at intersection of 4 cells inside grid - correct
+    L = [1.5,0]
+
+    #points at boundary corners of grids - correct
+    M = [-3, -3]
+    N = [3, -3]
+    O = [3, 1.5]
+    P = [-3, 1.5]
+    W = [-1.5,-4.5]
+    Z = [0, 3]
+
+    '''points at boundary cells intersection which means
+    a. top bottom exists at but either of lefts or rights are empty
+    b. left right exists but either of tops or bottoms are empty
+    '''
+    Q = [-3, 0]
+    R = [0, -3]
+    S = [3, -1.5]
+    T = [1.5, 1.5]
+    X = [-1.5, -4.5]
+
+    #points inside cells -- fine
+    U = [1.2, -1.2]
+    V = [-0.8, -2.2]
+
+    assert find_cell_id(A[1],A[0] , map) == 'C1'
+    assert find_cell_id(B[1], B[0], map) == 'C1'
+    assert find_cell_id(C[1], C[0], map) == 'C5'
+    assert find_cell_id(D[1], D[0], map) == 'C2'
+    assert find_cell_id(E[1], E[0], map) == 'C6'
+    assert find_cell_id(F[1], F[0], map) == 'C9'
+    assert find_cell_id(G[1], G[0], map) == 'C9'
+    assert find_cell_id(H[1], H[0], map) == 'C12'
+    assert find_cell_id(I[1], I[0], map) == 'C12'
+    assert find_cell_id(J[1], J[0], map) == 'C4'
+    assert find_cell_id(K[1], K[0], map) == 'C4'
+    assert find_cell_id(L[1], L[0], map) == 'C3'
+    assert find_cell_id(M[1], M[0], map) == 'C9'
+    assert find_cell_id(N[1], N[0], map) == 'C12'
+    assert find_cell_id(O[1], O[0], map) == 'C4'
+    assert find_cell_id(P[1], P[0], map) == 'C1'
+    assert find_cell_id(Q[1], Q[0], map) == 'C5'
+    assert find_cell_id(R[1], R[0], map) == 'C10'
+    assert find_cell_id(S[1], S[0], map) == 'C8'
+    assert find_cell_id(T[1], T[0], map) == 'C3'
+    assert find_cell_id(U[1], U[0], map) == 'C7'
+    assert find_cell_id(V[1], V[0], map) == 'C10'
+    assert find_cell_id(W[1], W[0], map) == 'C15'
+    assert find_cell_id(X[1], X[0], map) == 'C15'
+    assert find_cell_id(Y[1], Y[0], map) == 'C13'
+    assert find_cell_id(Z[1], Z[0], map) == 'C13'
+
+
+
